ce2fcdfa3d368ae4459db8b73c56db31b8f3c9a3
Arturo Silva <david.arturo.silva@gmail.com>
2022-06-04 04:21:05 -0400

    Web Site About the Universe

---
+++ src/app/topics/topics.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topics',
+  templateUrl: './topics.component.html',
+  styleUrls: ['./topics.component.css']
+})
+export class TopicsComponent implements OnInit {
+  topic = 'Photos of Mars';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
